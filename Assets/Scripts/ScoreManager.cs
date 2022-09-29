2c638d477587fae7501b97e4fed3e24be0832094
L-and <ksw1812@naver.com>
2022-09-29 14:54:40 +0900

    feat : ScoreManager
    GameManager에 있던 Score기능을 ScoreManager 스크립트로 작성

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int busStationCount; // 지나친 버스정류장의 수
+
+    private float score; // 점수
+    public float stationScore; // 정류장 점수
+
+    public float scoreMultiple; // 점수 배율
+
+    public float intervalTime;
+
+    private void Start()
+    {
+        score = 0; // 점수 초기화
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Player")
+        {
+            if (other.gameObject.tag == "BusStation")
+            {
+                print("정류장 경유");
+                busStationCount++;
+
+                float currentTime = Time.time;
+                score = (stationScore * scoreMultiple) / (currentTime - intervalTime);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (gameObject.tag == "Player")
+        {
+            if (other.gameObject.tag == "BusStation")
+            {
+                intervalTime = Time.time;
+            }
+        }
+    }
+}
