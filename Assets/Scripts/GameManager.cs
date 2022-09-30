70cbeafd3d020b7feee7a780d3a9b88184f86de6
L-and <ksw1812@naver.com>
2022-09-30 15:02:35 +0900

    fix : GameManager[wrong word]
    오타 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class GameManger : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     private GameObject[] unitGameObjects;
     private UnitMove[] unitMoves; // 모든 유닛들의 unitMove
+
     private PlayerController playerController;
 
     private bool _isGameRunning = false; // 게임실행시 true
@@ -18,7 +19,7 @@ public class GameManger : MonoBehaviour
         {
             _isGameRunning = !_isGameRunning;
 
-            if(_isGameRunning == true)
+            if (_isGameRunning == true)
                 activateComponents();
         }
     }
@@ -29,7 +30,7 @@ public class GameManger : MonoBehaviour
 
         unitMoves = new UnitMove[unitGameObjects.Length];
 
-        for(int i=0; i<unitGameObjects.Length;i++)
+        for (int i = 0; i < unitGameObjects.Length; i++)
         {
             unitMoves[i] = unitGameObjects[i].GetComponent<UnitMove>();
         }
@@ -42,28 +43,28 @@ public class GameManger : MonoBehaviour
 
     private void activateComponents()
     {
-        for(int i=0; i<unitMoves.Length; i++)
+        for (int i = 0; i < unitMoves.Length; i++)
         {
             unitMoves[i].enabled = true;
         }
         playerController.enabled = true;
     }
-    
-    
+
+
 }
 
 
 
 // 에디터에서 bool변수 수정을 위한 스크립트
 #if UNITY_EDITOR
-[CustomEditor(typeof(GameManger))]
-public class GameMangerEditor : Editor
+[CustomEditor(typeof(GameManager))]
+public class GameManagerEditor : Editor
 {
-    public GameManger script;
+    public GameManager script;
 
     public void OnEnable()
     {
-        script = (GameManger)target;
+        script = (GameManager)target;
     }
 
     public override void OnInspectorGUI()
@@ -71,7 +72,7 @@ public class GameMangerEditor : Editor
         bool is_isGameRunning = !script.isGameRunning;
         GUI.backgroundColor = (is_isGameRunning ? Color.red : Color.green);
 
-        if(GUILayout.Button("isGameRunning is"+script.isGameRunning+"(Click to make "+is_isGameRunning+")"))
+        if (GUILayout.Button("isGameRunning is" + script.isGameRunning + "(Click to make " + is_isGameRunning + ")"))
         {
             script.isGameRunning = is_isGameRunning;
         }
