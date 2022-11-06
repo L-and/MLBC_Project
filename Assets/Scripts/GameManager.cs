8463c8aa2c0f6fcc296a78d8c87db062beb3f938
L-and <ksw1812@naver.com>
2022-11-07 05:55:40 +0900

    fix : GameManager
    게임매니저에 오브젝트풀매니저들 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,10 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null; // 싱글톤패턴
 
-    private GameObject[] unitGameObjects;
-    private UnitMove[] unitMoves; // 모든 유닛들의 unitMove
     private UnitMove playerUnitMove;
+
+    [SerializeField]
+    private GameObject[] objectPoolManagers;
 
     private PlayerController playerController;
 
@@ -23,7 +24,7 @@ public class GameManager : MonoBehaviour
             _isGameRunning = !_isGameRunning;
 
             if (_isGameRunning == true)
-                activateComponents();
+                ActivateComponents();
         }
     }
 
@@ -43,33 +44,35 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-///////////유니티 에디터/////////////
-# if UNITY_EDITOR
-        _isGameRunning = true;
-# endif
-//////////////////////////////////
-        unitGameObjects = GameObject.FindGameObjectsWithTag("Unit");
+        GameObject player = GameObject.Find("Player");
+        playerController = player.GetComponent<PlayerController>();
+        playerUnitMove = player.GetComponent<UnitMove>();
 
-        unitMoves = new UnitMove[unitGameObjects.Length];
-
-        for (int i = 0; i < unitGameObjects.Length; i++)
-        {
-            unitMoves[i] = unitGameObjects[i].GetComponent<UnitMove>();
-        }
-        playerUnitMove = gameObject.GetComponent<UnitMove>();
-
-        playerController = gameObject.GetComponent<PlayerController>();
+        DisableComponents();
     }
 
-    private void activateComponents()
+    private void ActivateComponents()
     {
-        for (int i = 0; i < unitMoves.Length; i++)
-        {
-            unitMoves[i].enabled = true;
-        }
         playerUnitMove.enabled = true;
 
         playerController.enabled = true;
+
+        for(int i=0; i< objectPoolManagers.Length; i++)
+        {
+            objectPoolManagers[i].SetActive(true);
+        }
+    }
+
+    private void DisableComponents()
+    {
+        playerUnitMove.enabled = false;
+
+        playerController.enabled = false;
+
+        for (int i = 0; i < objectPoolManagers.Length; i++)
+        {
+            objectPoolManagers[i].SetActive(false);
+        }
     }
 
 
