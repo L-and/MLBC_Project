be185ba6b9a7aae620abd8bb2d9d9b8472b87afc
L-and <ksw1812@naver.com>
2023-11-11 15:19:47 +0900

    feat: 튜툐리얼 작업중

--- Assets/Scripts/UnitCrossChecker.cs
+++ Assets/Scripts/UnitCrossChecker.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 // 플레이어와 유닛이 스쳤는지 검사하는 스크립트
 public class UnitCrossChecker : MonoBehaviour
 {
+    public UnityEvent tutorialEvent;
+
+    private void Start()
+    {
+        tutorialEvent = new UnityEvent();
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         string tag = other.tag;
@@ -12,6 +20,7 @@ public class UnitCrossChecker : MonoBehaviour
         if (tag == "Unit")
         {
             FeverManager.AddFeverValue();
+            tutorialEvent.Invoke();
         }
     }
 }
