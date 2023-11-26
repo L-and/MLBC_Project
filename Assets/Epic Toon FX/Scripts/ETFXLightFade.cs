fae021088354776409c9368af437195bfecdcc20
HuiEon Lee <61311298+JEIHAA@users.noreply.github.com>
2023-11-27 00:37:02 +0900

    Epic Toon FX 에셋 추가

--- Assets/Epic Toon FX/Scripts/ETFXLightFade.cs
+++ Assets/Epic Toon FX/Scripts/ETFXLightFade.cs
@@ -1,38 +1,49 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 namespace EpicToonFX
 {
     public class ETFXLightFade : MonoBehaviour
     {
+        public enum OnLifeEnd { DoNothing, Disable, Destroy }
+
         [Header("Seconds to dim the light")]
         public float life = 0.2f;
-        public bool killAfterLife = true;
+        public OnLifeEnd onLifeEnd = OnLifeEnd.Destroy;
 
         private Light li;
         private float initIntensity;
 
         // Use this for initialization
-        void Start()
+        private void Start()
         {
-            if (gameObject.GetComponent<Light>())
+            li = GetComponent<Light>();
+            if (li != null)
             {
-                li = gameObject.GetComponent<Light>();
                 initIntensity = li.intensity;
             }
-            else
-                print("No light object found on " + gameObject.name);
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
-            if (gameObject.GetComponent<Light>())
+            if (li != null)
             {
                 li.intensity -= initIntensity * (Time.deltaTime / life);
-                if (killAfterLife && li.intensity <= 0)
-                    //Destroy(gameObject);
-					Destroy(gameObject.GetComponent<Light>());
+                if (li.intensity <= 0f)
+                {
+                    switch (onLifeEnd)
+                    {
+                        case OnLifeEnd.DoNothing:
+                            // Do nothing
+                            break;
+                        case OnLifeEnd.Disable:
+                            li.enabled = false;
+                            break;
+                        case OnLifeEnd.Destroy:
+                            Destroy(li);
+                            break;
+                    }
+                }
             }
         }
     }
